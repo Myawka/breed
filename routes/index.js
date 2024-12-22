27dc3c5fbd624f6194631b502e994978a107ef69
myawka <ssscolllo@gmail.com>
2024-12-22 14:18:08 +0300

    Пункт 4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/manchkin', function(req, res, next) {
+  res.send("<h1>Страница Манчкина</h1>")
+  });
+
+router.get('/meinkun', function(req, res, next) {
+  res.send("<h1>Страница Мейнкуна</h1>")
+    });
+
+router.get('/ragdoll', function(req, res, next) {
+  res.send("<h1>Страница Рэгдолл</h1>")
+    });
+
+router.get('/sfinks', function(req, res, next) {
+  res.send("<h1>Страница Сфинкса</h1>")
+
+  });
+
+router.get('/siamskaya', function(req, res, next) {
+  res.send("<h1>Страница Сиамской</h1>")
+    });
+
+router.get('/vislouhaya', function(req, res, next) {
+  res.send("<h1>Страница вислоухой</h1>")
+    });    
+
 module.exports = router;
