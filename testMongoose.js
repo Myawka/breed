4d88ce78b135f03cc98144d7c0de04ed0a27bb13
myawka <ssscolllo@gmail.com>
2025-01-01 20:00:35 +0300

    Пункт 7.1

--- testMongoose.js
+++ testMongoose.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 const Breed = mongoose.model('Breed', { name: String });
 const kitty = new Breed({ name: 'Кошка' });
-kitty.save().then(() => console.log('Мур'));
+kitty.save().then(() => console.log('Мур-мяу'));
