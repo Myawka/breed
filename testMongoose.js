91a68a2e9c01de2b16a005cd7f333765c216e075
myawka <ssscolllo@gmail.com>
2025-01-01 20:00:04 +0300

    breed_7_1

---
+++ testMongoose.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+const Breed = mongoose.model('Breed', { name: String });
+const kitty = new Breed({ name: 'Кошка' });
+kitty.save().then(() => console.log('Мур'));
