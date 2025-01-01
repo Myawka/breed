6cba95751fc265706ef90b3fbe7bdcaf2a780a5c
myawka <ssscolllo@gmail.com>
2025-01-01 20:57:05 +0300

    Пункт 7.3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.meow = function(){
-console.log(this.name + " мурлычет")
-}
-const Breed = mongoose.model('Breed', schema);
-const cat = new Breed({ name: 'Кошка' });
-cat.save().then(() => cat.meow());
+var Breed = require('./models/breed.js').Breed
+
+var breed = new Breed({
+    title: "Манчкин",
+    nick: "manchkin",
+})
+
+breed.save();
