6ec72bb6ab52b123479f6845c4bc6078c921bb06
myawka <ssscolllo@gmail.com>
2025-01-19 19:25:38 +0300

    Пункт 9.4

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.use(session({
   store: MongoStore.create({mongoUrl:
     'mongodb://localhost/breed2024'})
   }))
-  
+
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+  })
+    
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/breeds', breeds);
