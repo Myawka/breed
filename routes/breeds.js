57af8d4a00e3e825e575b8bf824b7aa952cace82
myawka <ssscolllo@gmail.com>
2025-01-19 13:07:10 +0300

    Пункт 8.3

--- routes/breeds.js
+++ routes/breeds.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Breed = require('../models/breed').Breed;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с breeds');
 });
 
 /* Страница пород */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });
+router.get("/:nick", async function (req, res, next) {
+  var breeds = await Breed.find({ nick: req.params.nick });
+  console.log(breeds)
+  if (!breeds.length) return next(new Error("Порода не найдена на сайте"))
+  var breed = breeds[0];
+  res.render('breed', {
+    title: breed.title,
+    picture: breed.avatar,
+    desc: breed.desc
+  })
+});
 
 module.exports = router;
